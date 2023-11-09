18342a9430c1f56d91cc7aec26b11495f5811fc0
Imagion <algernoncowperwood@gmail.com>
2023-11-09 21:22:11 +0300

    not perfect but working

--- src/app.js
+++ src/app.js
@@ -1,13 +1,36 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-<div class="flex flex-col items-center w-[min(100%,40rem)] bg-white p-5 rounded-3xl [&>*+*]:mt-3">
-  <q class="self-start text-xl pl-6 border-l-4 border-fuchsia-300 h-[3.5rem]">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, excepturi!
-  </q>
-  <p class="self-end text-xl text-neutral-400 italic">Lorem</p>
-  <button class="bg-fuchsia-300 py-2 px-4 rounded-lg text-lg">
-    Another Quote
-  </button>
-</div>
-`
+async function getQuote() {
+  const res = await fetch('https://api.quotable.io/quotes/random')
+  return res.json()
+}
+
+async function newQuote() {
+  try {
+    const data = await getQuote()
+
+    const app = document.querySelector('#app')
+    const quote = document.createElement('div')
+    quote.classList.add('wrapper')
+    quote.innerHTML = `<q class="self-start text-xl pl-6 border-l-4 border-fuchsia-300 h-[3.5rem]">${data[0].content}</q>`
+
+    const author = document.createElement('p')
+    author.classList.add('author')
+    author.innerHTML = `${data[0].author}`
+
+    const button = document.createElement('button')
+    button.classList.add('button')
+    button.innerHTML = `Another Quote`
+    button.addEventListener('click', () => {
+      app.innerHTML = ''
+      newQuote()
+    })
+
+    quote.appendChild(author)
+    quote.appendChild(button)
+    app.appendChild(quote)
+  } catch (e) {
+    console.error(e)
+  }
+}
+newQuote()
